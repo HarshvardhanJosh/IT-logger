b73296417920cd87297983c45d1ab95a430e695f
HarshvardhanJoshi <Harshitjoshi001@gmail.com>
2021-05-30 23:12:03 +0530

    Connected to Redux

--- src/reducers/logReducers.js
+++ src/reducers/logReducers.js
@@ -1,13 +1,13 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
 
-const initalState = {
+const initialState = {
   logs: null,
   current: null,
   loading: false,
   error: null,
 };
 
-export default (state = initalState, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case GET_LOGS:
       return {
